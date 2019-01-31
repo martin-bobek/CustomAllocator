9c913f9961b956674e557adcdc3ad0c1789bee32
martin-bobek <martinbobek8@gmail.com>
2019-01-30 20:59:12 -0700

    Added template parameter to class with member. Attempting to deduce parameter from only member pointer. Code does not compile.

--- Source.cpp
+++ Source.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 
+template <typename T>
 class Referenced {
 public:
-	void PrintT() {
-		std::cout << "Hello there" << std::endl;
+	void PrintT(T var) {
+		std::cout << "Hello there " << var << std::endl;
 	}
 };
 
-template <auto> struct Reference;
-template <typename Class, void (Class::*MF)()> struct Reference<MF> {
+template <auto> class Reference;
+template <template <typename> class Class, typename T, void (Class<T>::*MF)()> class Reference<MF> {
 public:
-	void Trigger(Class *instance) {
-		(instance->*MF)();
+	void Trigger(Class<T> *instance, T var) {
+		(instance->*MF)(var);
 	}
 };
 
-Referenced target;
+Referenced<size_t> target;
 
 int main() {
-	Reference<&Referenced::PrintT> ref;
-	ref.Trigger(&target);
+	Reference<&Referenced<size_t>::PrintT> ref;
+	ref.Trigger(&target, 42);
 }
