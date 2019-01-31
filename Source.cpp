0cebd3bb107a936cee4d8c439adbdd0457a09286
martin-bobek <martinbobek8@gmail.com>
2019-01-30 21:45:49 -0700

    Added a path to pass a variable of the nested template parameter type to the overall type.

--- Source.cpp
+++ Source.cpp
@@ -4,26 +4,27 @@ template <typename T>
 class A {
 public:
 	A(T var) : var(var) {}
-	void Print() {
-		std::cout << var << std::endl;
+	void Print(T adder) {
+		std::cout << var + adder << std::endl;
 	}
 private:
 	T var;
 };
 
-template <typename T>
-class B {
+template <typename> class B;
+template <template<typename> class Class, typename T>
+class B<Class<T>> {
 public:
-	B(T var) : var(var) {}
-	void Print() {
-		var.Print();
+	B(Class<T> var) : var(var) {}
+	void Print(T adder) {
+		var.Print(adder);
 	}
 private:
-	T var;
+	Class<T> var;
 };
 
 B<A<size_t>> global(42);
 
 int main() {
-	global.Print();
+	global.Print(7);
 }
