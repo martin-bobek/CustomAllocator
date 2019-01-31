cca5ee6332de6ed23f862039a0b810e8a07bcee8
martin-bobek <martinbobek8@gmail.com>
2019-01-30 21:39:13 -0700

    Created an example of nested templating.

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,29 @@
+#include <iostream>
 
+template <typename T>
+class A {
+public:
+	A(T var) : var(var) {}
+	void Print() {
+		std::cout << var << std::endl;
+	}
+private:
+	T var;
+};
+
+template <typename T>
+class B {
+public:
+	B(T var) : var(var) {}
+	void Print() {
+		var.Print();
+	}
+private:
+	T var;
+};
+
+B<A<size_t>> global(42);
 
 int main() {
-
+	global.Print();
 }
