dcc6fd2f94b1b7621c11555175c1d81e2c916dd9
martin-bobek <martinbobek8@gmail.com>
2019-01-30 19:56:15 -0700

    Demonstrated passing a pointer to a static object as a template parameter.

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,25 @@
+#include <iostream>
 
+//template <typename T>
+class Referenced {
+public:
+	void PrintT() {
+		std::cout << "Hello there" << std::endl;
+	}
+};
+
+template <Referenced &ref>
+class Reference {
+public:
+	void Trigger() {
+		ref.PrintT();
+	}
+};
+
+Referenced target;
 
 int main() {
-
+	Reference<target> ref;
+	ref.Trigger();
+	system("pause");
 }
