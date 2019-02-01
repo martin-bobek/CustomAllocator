9b6ad739b7cdce6ab15121d2aeb182401d271816
martin-bobek <martinbobek8@gmail.com>
2019-02-01 00:15:14 -0700

    Demonstrated accessing typedef in inherited class.

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
 
+class A {
+public:
+	typedef size_t Type;
+};
+
+class B : public A {
+public:
+	B(Type var) : var(var) {}
+	Type var;
+};
 
 int main() {
-
+	B b(43);
+	std::cout << b.var << std::endl;
+	system("pause");
 }
