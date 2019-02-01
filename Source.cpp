67cf58facc7e9cb3400084c0f049c70306bfd53b
martin-bobek <martinbobek8@gmail.com>
2019-01-31 22:48:09 -0700

    Working example of variadic template deduction from the community.

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,12 @@
+#include <map>
+#include <string>
 
+template <typename T, template<typename T, typename... Args> class C, typename... Args>
+C<T, Args...> foo() {
+	C<T, Args...> x;
+	return x;
+}
 
-int main() {
-
+int main(void) {
+	std::map<std::string, int> a = foo<std::string, std::map, int>();
 }
