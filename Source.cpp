716c21965a377f2e08960ba51745222f5ef301cc
martin-bobek <martinbobek8@gmail.com>
2019-02-01 00:31:01 -0700

    Used typedef from base class to deduce correct type in template accepting derived class.

--- Source.cpp
+++ Source.cpp
@@ -10,11 +10,18 @@ template <typename T>
 class B : public A<T> {
 public:
 	B(typename A<T>::Type var) : var(var) {}
+	void addPrint(typename A<T>::Type adder) const { std::cout << var + adder << std::endl; }
+private:
 	typename A<T>::Type var;
 };
 
+template <typename T>
+void Printer(const T &print, typename T::Type adder) {
+	print.addPrint(adder);
+}
+
 int main() {
 	B<size_t> b(43);
-	std::cout << b.var << std::endl;
+	Printer(b, 4);
 	system("pause");
 }
