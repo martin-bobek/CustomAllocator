84ef7a8320d02dcb5b2444f3868e3e6d9ee51868
martin-bobek <martinbobek8@gmail.com>
2019-02-02 00:02:18 -0700

    Enforced inheritance from A in type deduction of Printer to make Printer SFINAE friendly. Simpified type B.

--- Source.cpp
+++ Source.cpp
@@ -4,24 +4,34 @@ template <typename T>
 class A {
 public:
 	typedef T Type;
+	virtual void addPrint(T adder) const = 0;
+	virtual ~A() = default;
 };
 
 template <typename T>
 class B : public A<T> {
 public:
-	B(typename A<T>::Type var) : var(var) {}
-	void addPrint(typename A<T>::Type adder) const { std::cout << var + adder << std::endl; }
+	B(T var) : var(var) {}
+	void addPrint(T adder) const { std::cout << var + adder << std::endl; }
 private:
-	typename A<T>::Type var;
+	T var;
+};
+
+class C {
+public:
+	typedef size_t Type;
+	void addPrint(size_t adder) const { std::cout << "Error!" << std::endl; }
 };
 
 template <typename T>
-void Printer(const T &print, typename T::Type adder) {
+void Printer(T const &print, typename std::enable_if_t<std::is_convertible<T *, A<typename T::Type> *>::value, T>::Type adder) {
 	print.addPrint(adder);
 }
 
 int main() {
 	B<size_t> b(43);
+	C c;
 	Printer(b, 4);
+//  Printer(c, 7);    // Causes type deduction failure because C does not inherit from A.
 	system("pause");
 }
