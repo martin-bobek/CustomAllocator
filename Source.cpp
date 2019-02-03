b383a0359aa8278aeb79c48ed33e762c0648aa23
martin-bobek <martinbobek8@gmail.com>
2019-02-02 17:08:57 -0700

    Example of SFINAE function overloading.

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,40 @@
+#include <iostream>
+#include <type_traits>
+#include <vector>
 
+class Convertible {
+public:
+	Convertible() = default;
+	Convertible(int val) : val(val) {}
+	Convertible(Convertible &&) = default;
+	Convertible &operator=(Convertible &&) = default;
+	Convertible(const Convertible &) = default;
+	Convertible &operator=(const Convertible &) = default;
+	void Refresh(int val) {
+		this->val = val;
+		vec.clear();
+	}
+	operator int() { return val; }
+private:
+	int val;
+	std::vector<size_t> vec;
+};
+
+template <typename T, typename std::enable_if_t<std::is_integral_v<T>> * = nullptr>
+auto add(T lhs, T rhs) {
+	std::cout << "In template add" << std::endl;
+	return lhs + rhs;
+}
+
+Convertible add(int lhs, Convertible &&rhs) {
+	std::cout << "In optimized add" << std::endl;
+	rhs.Refresh(lhs + rhs);
+	return std::move(rhs);
+}
 
 int main() {
-
+	Convertible lhs(1), rhs(6), result;
+	result = add(lhs, std::move(rhs));
+	auto r = add(4, 3);
+	system("pause");
 }
